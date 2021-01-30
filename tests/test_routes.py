a6074b78600fed322fbfe42d06b51568fd3d5e11
Arvind <varvind@tamu.edu>
2021-01-30 15:40:24 -0600

    Added some initial unit tests for our routes to check if Travis passes

---
+++ tests/test_routes.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import json
+
+import sys
+sys.path.append('.')
+
+
+from app import configure_routes
+
+def test_hello_world():
+  app = Flask(__name__)
+  configure_routes(app)
+  client = app.test_client()
+  url = '/'
+
+  response = client.get(url)
+  print(response.get_data())
+  assert response.get_data() == 'Hello, World!'
+  assert response.status_code == 200
+
+def test_404():
+  app = Flask(__name__)
+  configure_routes(app)
+  client = app.test_client()
+  url = '/404'
+
+  response = client.get(url)
+  print(response.get_data())
+  assert response.get_data() == '404'
+  assert response.status_code == 200
