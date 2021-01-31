83d92c40662c0826d4eab2099212ed4b0e67dff3
qromanoski <quentinromanoski@gmail.com>
2021-01-31 12:10:11 -0600

    notes on /play endpoint

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+@app.route('/play')
+def play():
+    # create get method that sends current game's data and schedule of game
+    # create post method that takes in bid
+    return
+
 @app.route('/404')
 def route_404():
     return '404'
