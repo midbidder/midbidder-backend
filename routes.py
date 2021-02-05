7a7a1fd8d49ee20e392ec16e2e085759d4ea4cfb
Arvind <varvind@tamu.edu>
2021-02-05 16:33:15 -0600

    Added connection to mongodb atlas

---
+++ routes.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import os
+client = MongoClient(os.getenv('DATABASE_CONNECTION_STRING'))
+db = client['midbidder_backend']
+
+def configure_routes(app):
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    @app.route('/404')
+    def route_404():
+        return '404'
+
+    if __name__ == "__main__":
+        app.run(debug=True)
