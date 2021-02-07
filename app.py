19c0e25290678a0bf66ee0c98ab1bbaa80710793
qromanoski <quentinromanoski@gmail.com>
2021-02-07 13:48:12 -0600

    Figured out loggin

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask
+from flask import request
+import logging
 
 app = Flask(__name__)
-
+app.logger.setLevel(logging.INFO)
 
 def configure_routes(app):
-    @app.route('/')
+    @app.route('/', methods=['GET'])
     def hello_world():
-        return 'Hello, World!'
+        app.logger.info("This is the home page")
+        return 'Hello, World!!'
 
     @app.route('/404')
     def route_404():
         return '404'
+    
+    @app.route('/signin', methods=['POST'])
+    def sign_in():
+        app.logger.info(request.to_json())
+        app.logger.info("Hello")
+        google_user = request.args.get('tokenId')
+        return "hello"
 
     if __name__ == "__main__":
-        app.run(debug=True)
+        app.run(debug=True, port=5000)
+
+configure_routes(app)
+app.run()
