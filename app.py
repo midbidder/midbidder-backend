ff6dd6bf4d8b2d90c9169513af0dd678fc455241
qromanoski <quentinromanoski@gmail.com>
2021-02-08 12:51:03 -0600

    Fixed

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-
-def configure_routes(app):
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
-
-    @app.route('/404')
-    def route_404():
-        return '404'
-
-    if __name__ == "__main__":
-        app.run(debug=True)
+from routes import configure_routes
+configure_routes(app)
