1f12b9f599e81cd4e94df98cf1b78d13edd7f029
qromanoski <quentinromanoski@gmail.com>
2021-02-08 13:36:37 -0600

    Basic communication between front and backend; front sends token and backend validates and sends back user info

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS
 from routes import configure_routes
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
 configure_routes(app)
--- routes.py
+++ routes.py
@@ -1,4 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
-from routes import configure_routes
-configure_routes(app)
+from flask import request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from google.oauth2 import id_token
+from google.auth.transport import requests
+import os
+
+client = MongoClient(os.getenv('DATABASE_CONNECTION_STRING'))
+db = client['midbidder_backend']
+
+load_dotenv()
+
+def configure_routes(app):
+    @app.route('/', methods=['GET'])
+    def hello_world():
+        app.logger.info("This is the home page")
+        return 'Hello, World!!'
+
+    @app.route('/404')
+    def route_404():
+        return '404'
+    
+    @app.route('/signin', methods=['GET','POST', 'OPTIONS'])
+    def sign_in():
+        data = request.get_json()
+        if(data):
+            idinfo = id_token.verify_oauth2_token(data['id_token'], requests.Request(), os.getenv('FLASK_APP_GOOGLE_AUTH_CLIENT_ID'))
+            userid = idinfo['sub']
+            print(userid)
+            return userid
+        else:
+            return "Did not get Data"
+
+    if __name__ == "routes":
+        app.run(debug=True, port=5000)
