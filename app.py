abb00cc93f3e6ae85e02eafae75effd51d68d0ac
qromanoski <quentinromanoski@gmail.com>
2021-02-08 11:35:41 -0600

    Set up basic connection with front end sign in

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 import logging
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+load_dotenv()
+
 
 def configure_routes(app):
     @app.route('/', methods=['GET'])
@@ -15,12 +22,16 @@ def configure_routes(app):
     def route_404():
         return '404'
     
-    @app.route('/signin', methods=['POST'])
+    @app.route('/signin', methods=['GET','POST', 'OPTIONS'])
     def sign_in():
-        app.logger.info(request.to_json())
-        app.logger.info("Hello")
-        google_user = request.args.get('tokenId')
-        return "hello"
+        data = request.get_json()
+        if(data):
+            idinfo = id_token.verify_oauth2_token(data['id_token'], requests.Request(), os.getenv('FLASK_APP_GOOGLE_AUTH_CLIENT_ID'))
+            userid = idinfo['sub']
+            print(userid)
+            return userid
+        else:
+            return "Did not get Data"
 
     if __name__ == "__main__":
         app.run(debug=True, port=5000)
