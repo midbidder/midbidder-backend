fcf8c8b73b4c395df0b5131208e2350a428c9c3c
Arvind <varvind@tamu.edu>
2021-02-11 13:02:59 -0600

    hot fixing develop

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
-from routes import configure_routes
-configure_routes(app)
+from pymongo import MongoClient
+import os
+client = MongoClient(os.getenv('DATABASE_CONNECTION_STRING'))
+db = client['midbidder_backend']
+
+def configure_routes(app):
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    @app.route('/404')
+    def route_404():
+        return '404'
+
+    if __name__ == "__main__":
+        app.run(debug=True)
+
